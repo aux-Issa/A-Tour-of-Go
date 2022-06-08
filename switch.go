63df7ed5728cb817bfeec772d746c866f938f190
aux-Issa <issa1116184028@gmail.com>
2022-06-08 12:33:57 +0900

    SWITCH

--- switch.go
+++ switch.go
@@ -1,20 +1,18 @@
 package main
-
 import (
 	"fmt"
 	"runtime"
 )
 
 func main() {
-	fmt.Print("Go runs on ")
-	switch os := runtime.GOOS; os {
+	fmt.Print("Go runs on")
+	os := runtime.GOOS
+	switch os {
 	case "darwin":
 		fmt.Println("OS X.")
 	case "linux":
 		fmt.Println("Linux.")
 	default:
-		// freebsd, openbsd,
-		// plan9, windows...
-		fmt.Printf("%s.\n", os)
+		fmt.Printf("%s.\n", os)		
 	}
 }
